bc1f67331260f931f8f4cae8e78c02154883aafb
Администратор <denega.1975@yahoo.com>
2016-10-11 00:21:57 +0300

    777

--- src/main/java/shop/controllers/Admin_panelController.java
+++
@@ -1,7 +0,0 @@
-package shop.controllers;
-
-/**
- * Created by Администратор on 05.09.2016.
- */
-public class Admin_panelController {
-}
--- src/main/java/shop/controllers/HomeController.java
+++ src/main/java/shop/controllers/HomeController.java
@@ -1,7 +1,53 @@
 package shop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import shop.entity.User;
+import shop.services.UserService;
+
+import java.util.List;
+
 /**
  * Created by Администратор on 05.09.2016.
  */
+@Controller
 public class HomeController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(){
+        return "home";
+
+    }
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contactUs()
+    {
+        return "contactUs";
+    }
+
+    @RequestMapping(value = "about", method = RequestMethod.GET)
+    public String aboutUs(){
+        return "aboutUs";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPanel(){
+        return "adminPanel";
+    }
+
+    @RequestMapping(value = "/allUsers", method = RequestMethod.GET)
+    public  String allUsers(Model model){
+        List<User> userList = userService.findAll();
+        model.addAttribute("users", userList);
+        return "allUsers";
+    }
+    @RequestMapping(value = "/socialPage", method = RequestMethod.GET)
+    public String social(){
+        return "socialPage";
+    }
+
 }
